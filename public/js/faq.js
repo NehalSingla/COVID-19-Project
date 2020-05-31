72b54fe3446ac2525a29e752e8e4677e2a8b152f
Nehal Singla <nehalsingla24@gmail.com>
2020-05-31 23:00:33 +0530

    added faq

---
+++ public/js/faq.js
@@ -0,0 +1,8 @@
+ var faq= document.getElementById('faq');
+
+ faq.addEventListener('click',function()
+ {
+ 	
+ 	window.location="/faq_page";
+
+ })
--- public/js/test.js
+++ public/js/test.js
@@ -3,17 +3,5 @@ var test= document.getElementById('test');
 
 test.addEventListener('click',function()
 {
-	//console.log('hello');
-	var request= new XMLHttpRequest();
-	request.open('GET','/test');
-    //request.setRequestHeader('Content-Type',"application/json");
-
-	request.send();
-	request.addEventListener('load',function()
-	 {
- 	console.log('Data Saved Successfully');
-	// alert('New Vehicle Registered');
-	 });
-
 	window.location="/chat_page";
 })
